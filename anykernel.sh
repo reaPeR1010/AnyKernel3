072dab42bc194c40e68c09ca6e3615c1935b37b0
RoHaN <82774471+reaPeR1010@users.noreply.github.com>
2025-10-22 10:45:27 +0530

    AK3: Update string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=MiatollKernel
+kernel.string=Nexus Kernel for Miatoll Devices
 do.devicecheck=1
 do.modules=0
 do.systemless=1
